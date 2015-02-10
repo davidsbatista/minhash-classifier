7036fc7566ac5831ac96f846ed345b035d8b3dba
davidsbatista <dsbatista@gmail.com>
2015-02-10 18:38:13 +0000

    added train with all dataset, and k-fold train/test

--- classifier/postagger/train.py
+++ classifier/postagger/train.py
@@ -6,68 +6,92 @@ __author__ = 'dsbatista'
 import pickle
 from mxpost import MaxentPosTagger
 from nltk.corpus import ConllCorpusReader
+from sklearn.cross_validation import KFold
 
 
-def demo(corpus, num_sents):
-    """
-    Loads a few sentences from the Brown corpus or the Wall Street Journal
-    corpus, trains them, tests the tagger's accuracy and tags an unseen
-    sentence.
+def train_k_fold(sentences):
+    kf = KFold(len(sentences), 5)
+    overall_results = dict()
+    overall_results['adjective'] = 0
+    overall_results['adverb'] = 0
+    overall_results['conjunction'] = 0
+    overall_results['determiner'] = 0
+    overall_results['electronic'] = 0
+    overall_results['interjection'] = 0
+    overall_results['noun'] = 0
+    overall_results['numeral'] = 0
+    overall_results['preposition'] = 0
+    overall_results['pronoun'] = 0
+    overall_results['punctuation'] = 0
+    overall_results['symbol'] = 0
+    overall_results['verb'] = 0
+    overall_results['verb_past'] = 0
 
-    @type corpus: C{str}
-    @param corpus: Name of the corpus to load, either C{brown} or C{treebank}.
+    fold = 0
+    for train_index, test_index in kf:
+        print "\nFOLD", fold
+        train = []
+        test = []
 
-    @type num_sents: C{int}
-    @param num_sents: Number of sentences to load from a corpus. Use a small
-    number, as training might take a while.
-    """
-    if corpus.lower() == "brown":
-        from nltk.corpus import brown
-        tagged_sents = brown.tagged_sents()[:num_sents]
+        print "TRAIN", train_index, len(train_index)
+        print "TEST", test_index, len(test_index)
 
-    elif corpus.lower() == "treebank":
-        from nltk.corpus import treebank
-        tagged_sents = treebank.tagged_sents()[:num_sents]
+        for index in train_index:
+            train.append(sentences[index])
 
-    elif corpus.lower() == "floresta":
-        from nltk.corpus import floresta
-        tagged_sents = floresta.tagged_sents()[:num_sents]
+        for index in test_index:
+            test.append(sentences[index])
 
-    elif corpus.lower() == "cintil":
-        print "Loading CINTIL"
-        #column_types = ['ignore','words','ignore','ignore','pos','ignore']
-        #cintil = ConllCorpusReader('/home/dsbatista/cintil/','cintil-fixed.conll',column_types)
-        column_types = ['words', 'pos', 'ignore']
-        #cintil = ConllCorpusReader('/home/dsbatista/minhash-classifier/postagger','cintil-fixed.conll',column_types)
-        cintil = ConllCorpusReader('/home/dsbatista/minhash-classifier/classifier/postagger/datasets', 'cintil-fixed-reduced.conll', column_types)
-        tagged_sents = cintil.tagged_sents()[:num_sents]
+        maxent_tagger = MaxentPosTagger()
+        print "Training"
+        maxent_tagger.train(train)
+        print "Testing"
+        correct_tags, total_tags = maxent_tagger.evaluate(test)
 
-    else:
-        print "Please load either the 'brown' or the 'treebank' corpus."
+        for tag in correct_tags:
+            print tag, ':\t'+str(correct_tags[tag]/float(total_tags[tag]))
+            overall_results[tag] += correct_tags[tag]/float(total_tags[tag])
 
-    #size = int(len(tagged_sents) * 0.1)
-    #train_sents, test_sents = tagged_sents[size:], tagged_sents[:size]
-    train_sents = cintil.tagged_sents()
-    test_sents = cintil.tagged_sents()
-    print train_sents,test_sents
+        print "Saving generated model"
+        f_model = open('pos-tagger_'+str(fold)+'_.pkl', "wb")
+        pickle.dump(maxent_tagger, f_model, 1)
+        f_model.close()
+
+        fold += 1
+
+    print "\nAccuracy on", kf.n_folds
+    for tag in overall_results:
+        print tag, overall_results[tag] / float(kf.n_folds)
+
+
+def train_all(sentences):
     maxent_tagger = MaxentPosTagger()
-    maxent_tagger.train(train_sents)
-    maxent_tagger.evaluate(test_sents)
+    print "Training"
+    maxent_tagger.train(sentences)
+
+    print "Saving generated model"
+    f_model = open('pos-tagger_all_.pkl', "wb")
+    pickle.dump(maxent_tagger, f_model, 1)
+    f_model.close()
+
+
+def main():
+    directory = "/home/dsbatista/PycharmProjects/minhash-classifier/classifier/postagger/datasets"
+    columns = ['words', 'pos', 'ignore']
+    print "Reading CINTIL corpus"
+    cintil = ConllCorpusReader(directory, 'CINTIL-CoNNL-reduced-past-verbs.txt', columns)
+    #cintil = ConllCorpusReader(directory, 'CINTIL-CoNNL-format.txt', columns)
+    sentences = cintil.tagged_sents()
+    print len(sentences), " sentences loaded"
+    train_all(sentences)
 
     """
-    print "tagger accuracy (test %i sentences, after training %i):" % \
-        (size, (num_sents - size)), maxent_tagger.evaluate(test_sents)
     print "\n\n"
-    print "classify unseen sentence: ", maxent_tagger.tag(["Isto", "é", "bastante","rápido", "!"])
+    print "classify unseen sentence: ", maxent_tagger.tag(["Isto", "é", "bastante", "rápido", "!"])
     print "\n\n"
     print "show the 40 most informative features:"
     print maxent_tagger.classifier.show_most_informative_features(40)
     """
 
-    f_model = open('test.pkl', "wb")
-    pickle.dump(maxent_tagger, f_model, 1)
-    f_model.close()
-
 if __name__ == '__main__':
-    demo("cintil", 100)
-
+    main()
