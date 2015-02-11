7be8de2049c45309df60257c1f3a9f2d03811cca
davidsbatista <dsbatista@gmail.com>
2015-02-11 19:32:38 +0000

    ReVerb pattern extration using mode trained with CINTIL reduced tagset preserving past participle tag

--- classifier/postagger/test.py
+++ classifier/postagger/test.py
@@ -1,80 +1,197 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import StringIO
+import codecs
 import re
 import pickle
 import sys
+from Sentence import Sentence
 
 tokenizer =  r'\w+(?:-\w+)+|\d+(?:[:|/]\d+)+|\d+(?:[.]?[oaºª°])+|\w+\'\w+|\d+(?:[,|.]\d+)*\%?|[\w+\.-]+@[\w\.-]+|https?://[^\s]+|\w+'
 
 
-def extract_reverb_patterns(text_tokenized):
-    tagged = tagger.tag(text_tokenized)
-    for tag in tagged:
-        print '/'.join(tag),
-    print "\n",
+def extract_reverb_patterns_ptb(text):
 
+    """
+    Extract ReVerb relational patterns
+    http://homes.cs.washington.edu/~afader/bib_pdf/emnlp11.pdf
+
+    # extract ReVerb patterns:
+    # V | V P | V W*P
+    # V = verb particle? adv?
+    # W = (noun | adj | adv | pron | det)
+    # P = (prep | particle | inf. marker)
+    """
+
+    # remove the tags and extract tokens
+    text_no_tags = re.sub(r"</?[A-Z]+>", "", text)
+    tokens = re.findall(tokenizer, text_no_tags.decode("utf8"), flags=re.UNICODE)
+
+    # tag tokens with pos-tagger
+    tagged = tagger.tag(tokens)
     patterns = []
+    patterns_tags = []
+    i = 0
+    limit = len(tagged)-1
+    tags = tagged
 
-    for i in range(0, len(tagged)):
-        word = tagged[i]
-        # if the word is a verb
-        if word[1] == 'verb':
-                pattern = tagged[i][0]
-                if i < len(tagged)-2:
-                    # ReVerb
-                    j = i+1
-                    while (j < (len(tagged)-2) and (
-                        tagged[j][1] == 'adjective' or   # adjectivs
-                        tagged[j][1] == 'adverb' or      # adverbs
-                        tagged[j][1] == 'determiner' or  # adverbs
-                        tagged[j][1] == 'verb' or        # verbs
-                        tagged[j][1] == 'noun')):        # noun
-                            pattern += '_'+tagged[j][0]
-                            j += 1
+    """
+    verb = ['VB', 'VBD', 'VBD|VBN', 'VBG', 'VBG|NN', 'VBN', 'VBP', 'VBP|TO', 'VBZ', 'VP']
+    adverb = ['RB', 'RBR', 'RBS', 'RB|RP', 'RB|VBG', 'WRB']
+    particule = ['POS', 'PRT', 'TO', 'RP']
+    noun = ['NN', 'NNP', 'NNPS', 'NNS', 'NN|NNS', 'NN|SYM', 'NN|VBG', 'NP']
+    adjectiv = ['JJ', 'JJR', 'JJRJR', 'JJS', 'JJ|RB', 'JJ|VBG']
+    pronoun = ['WP', 'WP$', 'PRP', 'PRP$', 'PRP|VBP']
+    determiner = ['DT', 'EX', 'PDT', 'WDT']
+    adp = ['IN', 'IN|RP']
+    """
 
-                    # must end in a proposition
-                    if j < (len(tagged)-2) and tagged[j][1] == "preposition":
-                        pattern += '_'+tagged[j][0]+'_RVB'
-                        patterns.append(pattern)
+    verb = ['verb', 'verb_past']
+    word = ['noun', 'adjective', 'adverb', 'determiner', 'pronoun']
+    particule = ['preposition']
 
-                    # negation detection
-                    if (i-1 > 0) and (tagged[i-1][0] == u"não" or
-                                      tagged[i-1][0] == u"nunca" or
-                                      tagged[i-1][0] == u"jamais"):
-                            neg_pattern = tagged[i-1][0]+'_'+pattern+'_RVB'
-                            patterns.append(neg_pattern)
+    """
+    conjunction
+    electronic
+    interjection
+    numeral
+    punctuation
+    symbol
 
-    for p in patterns:
-        print p
-    print "\n",
+    adjective
+    adverb
+    determiner
+    noun
+    preposition
+    pronoun
+    verb
+    verb_past
+    """
+
+    while i <= limit:
+        tmp = StringIO.StringIO()
+        tmp_tags = []
+
+        # a ReVerb pattern always starts with a verb
+        if tags[i][1] in verb:
+            tmp.write(tags[i][0]+' ')
+            t = (tags[i][0], tags[i][1])
+            tmp_tags.append(t)
+            i += 1
+
+            # V = verb particle? adv? (also capture auxiliary verbs)
+            while i <= limit and (tags[i][1] in verb or tags[i][1] in word):
+                tmp.write(tags[i][0]+' ')
+                t = (tags[i][0], tags[i][1])
+                tmp_tags.append(t)
+                i += 1
+
+            # W = (noun | adj | adv | pron | det)
+            while i <= limit and (tags[i][1] in word):
+                tmp.write(tags[i][0]+' ')
+                t = (tags[i][0], tags[i][1])
+                tmp_tags.append(t)
+                i += 1
+
+            # P = (prep | particle | inf. marker)
+            while i <= limit and (tags[i][1] in particule):
+                tmp.write(tags[i][0]+' ')
+                t = (tags[i][0], tags[i][1])
+                tmp_tags.append(t)
+                i += 1
+
+            # add the build pattern to the list collected patterns
+            patterns.append(tmp.getvalue())
+            patterns_tags.append(tmp_tags)
+        i += 1
+
+    return patterns, patterns_tags
+
+
+def extract_patterns(text):
+    patterns, patterns_tags = extract_reverb_patterns_ptb(text)
+    if len(patterns) > 0:
+        for pattern in patterns_tags:
+            for tag in pattern:
+                print '/'.join(tag).encode("utf8"),
+            print "\n"
+        print "\n"
 
 
 def process(sentence):
-    tokens = re.findall(tokenizer, sentence.decode("utf8"), flags=re.UNICODE)
-    extract_reverb_patterns(tokens)
+
+    s = Sentence(sentence)
+
+    for rel in s.relationships:
+        """
+        print rel.ent1
+        print rel.ent2
+        print rel.before
+        print rel.between
+        print rel.after
+        """
+        extract_patterns(rel.before)
+        extract_patterns(rel.between)
+        extract_patterns(rel.after)
+
+
+    """
+    if len(patterns) > 0:
+        for pattern in patterns_tags:
+            # detect passive-voice
+            for i in range(0, len(pattern)):
+                passive_voice = False
+                if pattern[i][1].startswith('v'):
+                    try:
+                        inf = verbs[pattern[i][0]]
+                        if inf in ['ser', 'estar', 'ter', 'haver'] and i+2 <= len(pattern)-1:
+                            if (pattern[-2][1] == 'verb_past' or pattern[-2][1] == 'adjectiv') and pattern[-1][0] == 'por':
+                                passive_voice = True
+                    except KeyError:
+                        continue
+
+                if passive_voice is True:
+                    #print "passive voice:",
+                    for tag in pattern:
+                        print '/'.join(tag).encode("utf8"),
+                    print "\n"
+    """
 
 
 def main():
+    global verbs
+    print "Loading Label-Delaf"
+    verbs_conj = open('../verbs/verbs_conj.pkl', "r")
+    verbs = pickle.load(verbs_conj)
+    verbs_conj.close()
+
     global tagger
     print "Loading PoS tagger\n"
     f_model = open(sys.argv[1], "rb")
     tagger = pickle.load(f_model)
     f_model.close()
 
-    sentence1 = """As acções do Banco Espírito Santo (BES) voltaram a negociar-se em bolsa."""
-    sentence2 = """Após ter suspendido a negociação dos títulos do banco."""
-    sentence3 = """O Governo de Ricardo foi acusado por um deputado do PS."""
-    sentence4 = """João fez duras críticas a Carlos Carvalhas."""
-    sentence5 = """Passos Coelho não fez qualquer tipo de declaração."""
-    sentence6 = """Passos Coelho chegou a Lisboa e não fez qualquer tipo de declação."""
+    #f_sentences = codecs.open(sys.argv[2], encoding='utf-8')
+    f_sentences = codecs.open(sys.argv[2])
+    for line in f_sentences:
+        process(line)
+    f_sentences.close()
 
+    """
+    sentence1 = "As acções do Banco Espírito Santo (BES) voltaram a negociar-se em bolsa."
+    sentence2 = "Após ter suspendido a negociação dos títulos do banco."
+    sentence3 = "O Governo de Ricardo foi acusado por um deputado do PS."
+    sentence4 = "João fez duras críticas a Carlos Carvalhas."
+    sentence5 = "Passos Coelho não fez qualquer tipo de declaração."
+    sentence6 = "Passos Coelho chegou a Lisboa e não fez qualquer tipo de declação."
     process(sentence1)
     process(sentence2)
     process(sentence3)
     process(sentence4)
     process(sentence5)
     process(sentence6)
+    """
 
 
 if __name__ == "__main__":
--- classifier/verbs/verb-normalizer-PT.py
+++ classifier/verbs/verb-normalizer-PT.py
@@ -7,20 +7,22 @@ import pickle
 
 verbs = dict()
 
-def loadVerbs():
-    label = "Label-Delaf_pt_v4_1.dic.utf8.gz";
-    f = gzip.open(label,'rb','utf-8')
+
+def load_verbs():
+    label = "Label-Delaf_pt_v4_1.dic.utf8.gz"
+    f = gzip.open(label, 'rb', 'utf-8')
     for line in f:
-        if (not line.startswith("%") and line.find(".V")!=-1):
-            conjugation,verb = line.split(".V")[0].split(",")
+        if not line.startswith("%") and line.find(".V")!=-1:
+            conjugation, verb = line.split(".V")[0].split(",")
             verbs[conjugation] = verb
-    print len(verbs.keys()),"loaded"
+    print len(verbs.keys()), "loaded"
+
 
 def main():
-    loadVerbs()
-    verbsConj = open('verbsConj.pkl',"wb")
-    pickle.dump(verbs, verbsConj,1)
-    verbsConj.close()
+    load_verbs()
+    verbs_conj = open('verbsConj.pkl',"wb")
+    pickle.dump(verbs, verbs_conj, 1)
+    verbs_conj.close()
 
 if __name__ == "__main__":
     main()
